eed0758e7d17ae5849c2372378787ebb6569beb8
Soban Saleem <wakeuptoreality9to5@gmail.com>
2024-08-01 01:37:31 -0700

    Add files via upload

---
+++ assignment01.py
@@ -0,0 +1,77 @@
+
+# 01 a) Basic Printing 
+
+print("hello world")
+
+# b)  Print your first name 
+
+print ("Soban")
+
+# 02 PRINT MULTIPLE ITEMS 
+# a) Print your first name and last name in a single print statement.
+
+print ("Soban", "Saleem")
+
+# b) Print number 1 2 3 
+
+print ("1,2,3")
+
+# 03 PRINT SPECIAL CHARACTER
+# a)Print a string that includes a newline character to display text on two lines.
+
+print (" Hello \n World")
+
+# b) Print a string that includes a tab character.
+
+print ("Hello\tWorld") 
+
+# 04 Using the sep Parameter
+# a) - Print three different words separated by commas. For example: 'apple, banana, cherry
+
+print("apple", "banana", "cherry", sep=", ")
+
+# b)  - Print three different numbers separated by hyphens. For example: '1-2-3'.
+
+print(1,2,3, sep="-")
+
+
+# 05 Using the end Parameter
+
+# a) Print two words on the same line with a space in between
+
+print("Hello", "World")
+
+# b) Print the numbers 1 and 2 on the same line without a space in between
+
+print("12")
+
+
+# 06 Escape Characters
+
+# a) Print a string that includes double quotes
+
+print('He said, "Hello!"')
+
+# b) Print a backslash character
+
+print("This is backslash: \\")
+
+
+# 07 Combining Text and Numbers
+
+# a) Print your age alongside a descriptive message
+
+print ("i am", 22, "years old")
+
+# b) Print a number and a string together
+print("the number is", 5)
+
+
+# 08  Basic Loop for Printing
+# - Print the numbers 1 to 5, each on a new line using separate print statements
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
