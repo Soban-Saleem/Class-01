cb4db058cb6005bc2967cfc8eedf705bb8a84dba
Soban Saleem <wakeuptoreality9to5@gmail.com>
2024-08-07 01:24:01 -0700

    Add files via upload
    Python Basic Practice  Assignment (Aug 04, 2024)

---
+++ Assignment02.py
@@ -0,0 +1,66 @@
+
+
+# PYTHON PRACTICE EXERCISE 
+
+# 01 Assign a message to Variable 
+
+msg = "Unama tina Misono al zato"
+
+# Printing the result
+print (msg)
+
+
+
+# 02 Change the value of the variable to a new message, and print the new message
+
+msg = "Unama tina Misono al zato"
+
+# Print the first message
+print (msg)
+
+# Change the value of the variable to a new message
+msg = "Bella Ciao, bella ciao, ciao ciao"
+
+# Print the new message
+print (msg)
+
+
+# 03 PERSONAL NAME 
+
+# a) Use a variable to represent a person’s name
+
+name = "Professor"
+
+# printing the name
+print (f"hello, {name} would you like to learn more about python ?")
+
+# 04 NAME CLASS
+
+# a) Use a variable to represent a person’s name
+# b) Print the person’s name in lowercase, uppercase, and title case
+
+names = "Berlin"
+
+#Lower case
+print (names.lower())
+
+#uppercase
+print (names.upper())
+
+#Title case
+print (names.title())
+
+
+# 05 FAMOUS QUOTE
+
+# a) Find a quote from a famous person you admire.
+
+quote = " The majic you are looking for, is in the work you are avoiding. "
+
+author = " professor "
+
+# b) Print the quote and the name of its author.
+
+print (f"{author} Once said \"{quote}\"")
+
+
